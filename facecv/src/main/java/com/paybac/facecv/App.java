846c3dc4ec799e06106f9cb932631a854fdf06f9
pscollins@uchicago.edu <pscollins@uchicago.edu>
2014-07-20 04:44:47 -0500

    hellishly overcomplicated default maven structure

---
+++ facecv/src/main/java/com/paybac/facecv/App.java
@@ -0,0 +1,13 @@
+package com.paybac.facecv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
